2a356f8f0870f6ba41536d89144d5743ceb2feae
Khan Lee <khan.lee@wclub.co.kr>
2024-02-26 17:30:52 +0900

    [FEAT] 예외처리 추가

--- review-check.py
+++ review-check.py
@@ -13,12 +13,15 @@ def send_slack_message(message):
     channel = 'U069RPHRU95'
     client.chat_postMessage(channel=channel, text=message)
 
+try:
+    # 이벤트 정보를 사용하여 메시지를 작성
+    pull_request_url = os.getenv('GITHUB_SERVER_URL') + '/' + os.getenv('GITHUB_REPOSITORY') + '/pull/' + os.getenv(
+        'PULL_REQUEST_NUMBER')
+    sender_username = os.getenv('GITHUB_ACTOR')
+    message = f"Pull request에 리뷰가 제출되었습니다. 리뷰어: {sender_username}, Pull Request: {pull_request_url}"
 
-# 이벤트 정보를 사용하여 메시지를 작성
-pull_request_url = os.getenv('GITHUB_SERVER_URL') + '/' + os.getenv('GITHUB_REPOSITORY') + '/pull/' + os.getenv(
-    'PULL_REQUEST_NUMBER')
-sender_username = os.getenv('GITHUB_ACTOR')
-message = f"Pull request에 리뷰가 제출되었습니다. 리뷰어: {sender_username}, Pull Request: {pull_request_url}"
+    # Slack으로 메시지 전송
+    send_slack_message(message)
 
-# Slack으로 메시지 전송
-send_slack_message(message)
+except Exception as e:
+    print('예외가 발생했습니다.', e)
