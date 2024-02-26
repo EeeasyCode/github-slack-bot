80e48ff6ad59490b2c23c20a827297a15da6afa4
Khan Lee <khan.lee@wclub.co.kr>
2024-02-26 17:52:48 +0900

    [FEAT] review check bot 생성

--- review-check.py
+++ review-check.py
@@ -1,27 +1,39 @@
-import os
+from github import Github
 from slack_sdk import WebClient
+from slack_sdk.errors import SlackApiError
 import ssl
 import certifi
+import os
+import json
 import dotenv
 
 dotenv.load_dotenv()
 
-def send_slack_message(message):
+
+g = Github(os.environ.get('Git_Token'))
+repo = g.get_user().get_repo(os.environ.get('Git_Repo_Name'))
+
+# 환경 변수에서 이벤트 페이로드 가져오기
+event_payload = json.loads(os.getenv('GITHUB_EVENT_PATH'))
+
+# 풀 리퀘스트 리뷰 이벤트인 경우
+pr_number = event_payload['pull_request']['number']
+pr = repo.get_pull(pr_number)
+pr_author = pr.user.login
+
+# Slack 메시지 작성
+message = f"안녕하세요, {pr_author}님! 풀 리퀘스트가 리뷰되었습니다."
+
+# Slack으로 메시지 보내기
+try:
     ssl_context = ssl.create_default_context(cafile=certifi.where())
     slack_token = os.environ.get('assign_slack_token')
     client = WebClient(token=slack_token, ssl=ssl_context)
-    channel = 'U069RPHRU95'
-    client.chat_postMessage(channel=channel, text=message)
-
-try:
-    # 이벤트 정보를 사용하여 메시지를 작성
-    pull_request_url = 'https://github.com/EeeasyCode/github-slack-bot/pull/' + os.getenv(
-        'PULL_REQUEST_NUMBER')
-    sender_username = os.getenv('GITHUB_ACTOR')
-    message = f"Pull request에 리뷰가 제출되었습니다. 리뷰어: {sender_username}, Pull Request: {pull_request_url}"
-
-    # Slack으로 메시지 전송
-    send_slack_message(message)
-
-except Exception as e:
-    print('예외가 발생했습니다.', e)
+    slack_id = 'U069RPHRU95'
+    client.chat_postMessage(
+        channel=slack_id,
+        text=message,
+    )
+    print("Slack 메시지가 성공적으로 전송되었습니다.")
+except SlackApiError as e:
+    print(f"Slack 메시지 전송 중 오류 발생: {e.response['error']}")
